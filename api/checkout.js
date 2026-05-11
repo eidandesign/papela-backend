896e16872210b4213cbadc838bb2e8b942e90d35
Adan Careta <adan@vervemarket.com>
2026-05-11 09:14:58 -0600

    debug error real

--- api/checkout.js
+++ api/checkout.js
@@ -96,6 +96,6 @@ export default async function handler(req, res) {
     })
   } catch (error) {
     console.error('[checkout] Error:', error.message)
-    return res.status(500).json({ error: error.message })
+    return res.status(500).json({ error: ' + error.message + ' })
   }
 }
